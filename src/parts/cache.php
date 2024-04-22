ca5667de0fbfbef2e52e589a56a4a9f4b63d81b7
Vea <vea.urn2@gmail.com>
2024-04-22 20:01:45 +0800

    add trait function:cacheInstance

--- src/parts/cache.php
+++ src/parts/cache.php
@@ -5,7 +5,7 @@ trait cache{
 	protected static array $_cache_model=[];
 	/**
 	 * 共享，不要在create中缓存single，或者加限定名 如id
-	 * @param string $ModelNameSpace
+	 * @param string $ModelNameSpace 缓存名字同命名空间名，不要缓存single类的
 	 * @param        ...$args
 	 * @return mixed
 	 */
@@ -14,5 +14,19 @@ trait cache{
 			self::$_cache_model[$ModelNameSpace]=new $ModelNameSpace(...$args);
 		}
 		return self::$_cache_model[$ModelNameSpace];
+	}
+	/**
+	 * 缓存实例用，会自动在缓存名后加入实例名，如 user~1 user~2，需自己指定实例名称
+	 * @param string $Namespace 模型名
+	 * @param string $instanceName 实例名，如 id
+	 * @param mixed ...$args 构造参数
+	 * @return mixed
+	 */
+	protected function cacheInstance(string $Namespace, string $instanceName, ...$args):mixed{
+		$name ="$Namespace~$instanceName";
+		if(!array_key_exists($name, self::$_cache_model)){
+			self::$_cache_model[$name] =new $Namespace(...$args);
+		}
+		return self::$_cache_model[$name];
 	}
 }

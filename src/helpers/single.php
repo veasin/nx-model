4c3d79e183c90aa0310aead431281a3fd53ca5c8
vea <vea@live.cn>
2025-01-03 23:16:51 +0800

    middleware:update create delete

--- src/helpers/single.php
+++ src/helpers/single.php
@@ -40,6 +40,15 @@ abstract class single{
 	protected function default(array $data=[]):void{
 		$this->plugin('default', $data);
 	}
+	protected function updateMiddleware($update=[]):\Generator{
+		return yield true;
+	}
+	protected function createMiddleware():\Generator{
+		return yield true;
+	}
+	protected function deleteMiddleware():\Generator{
+		return yield true;
+	}
 	/**
 	 * 保存当前对象中的数据，如不存在即添加
 	 * @return bool
@@ -47,22 +56,33 @@ abstract class single{
 	public function save():bool{
 		if($this->id){
 			$update=array_diff_assoc($this->data, $this->_data);//对比出需要更新数据
-			if(empty($update)) return true;//如果无须更新返回成功
-			if(static::MULTIPLE::FIELD_UPDATED) $update[static::MULTIPLE::FIELD_UPDATED]=time();
-			$ok=$this->table()->where([$this->id_key=>$this->id])->update($update)->execute()->ok();
-			$this->_data=$this->data;
-			if($ok) $this->plugin('update');
-		}else{
-			if(static::MULTIPLE::FIELD_CREATED && !array_key_exists(static::MULTIPLE::FIELD_CREATED, $this->data)) $this->data[static::MULTIPLE::FIELD_CREATED]=time();
-			$this->plugin('before_create');
-			$id=$this->table()->create($this->data)->execute()->lastInsertId();
-			$ok=$id > 0;
-			if($ok){
-				$this->data=$this->_data=$this->table()->where([$this->id_key=>$id])->select()->execute()->first();
-				$this->id=$this->data[$this->id_key];
-				$this->plugin('create');
-				$this->save();//触发二次保存逻辑
+			$middleware =$this->updateMiddleware($update);
+			if($middleware->current() && !empty($update)){
+				if(static::MULTIPLE::FIELD_UPDATED) $update[static::MULTIPLE::FIELD_UPDATED] = time();
+				$ok = $this->table()->where([$this->id_key => $this->id])->update($update)->execute()->ok();
 			}
+			$middleware->send($ok ?? true);
+			$ok=$middleware->getReturn();
+			if($ok){
+				$this->_data = $this->data;
+				$this->plugin('update', $update);
+			}
+		}else{
+			$this->plugin('before_create');
+			$middleware =$this->createMiddleware();
+			if($middleware->current()){
+				if(static::MULTIPLE::FIELD_CREATED && !array_key_exists(static::MULTIPLE::FIELD_CREATED, $this->data)) $this->data[static::MULTIPLE::FIELD_CREATED]=time();
+				$id=$this->table()->create($this->data)->execute()->lastInsertId();
+				$ok=$id > 0;
+				if($ok){
+					$this->data=$this->_data=$this->table()->where([$this->id_key=>$id])->select()->execute()->first();
+					$this->id=$this->data[$this->id_key];
+					$this->save();//触发二次保存逻辑
+				}
+			}
+			$middleware->send($ok ?? true);
+			$ok=$middleware->getReturn();
+			if($ok) $this->plugin('create');
 		}
 		return $ok;
 	}
@@ -72,12 +92,18 @@ abstract class single{
 	 */
 	public function delete():bool{
 		if($this->id > 0){
-			$this->_data=$this->data;
-			$table=$this->table()->where([$this->id_key=>$this->id]);
-			if(static::MULTIPLE::TOMBSTONE && static::MULTIPLE::FIELD_DELETED){//逻辑删除
-				$table->update([static::MULTIPLE::FIELD_DELETED=>time()]);
-			}else $table->delete();
-			$ok=$table->execute()->ok();
+			$middleware =$this->deleteMiddleware();
+			if($middleware->current()){
+				$this->_data = $this->data = [];
+				$table = $this->table()->where([$this->id_key => $this->id]);
+				if(static::MULTIPLE::TOMBSTONE && static::MULTIPLE::FIELD_DELETED){//逻辑删除
+					$table->update([static::MULTIPLE::FIELD_DELETED => time()]);
+				}
+				else $table->delete();
+				$ok = $table->execute()->ok();
+			}
+			$middleware->send($ok ?? true);
+			$ok=$middleware->getReturn();
 			if($ok) $this->plugin('delete');
 			return $ok;
 		}else{
